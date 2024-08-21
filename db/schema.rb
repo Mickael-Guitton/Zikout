8841dd0a98b240f463024542f2e40dd2941859cc
Alexis Reubrecht <lxsrbrcht@gmail.com>
2024-08-21 15:44:56 +0200

    venues kinda, needs work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_21_090122) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_21_132903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_21_090122) do
     t.string "paying"
     t.integer "capacity"
     t.bigint "user_id"
+    t.string "category"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
