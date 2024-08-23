2878a10e44b555dd88024c03a6a5853327f6499a
Alexis Reubrecht <lxsrbrcht@gmail.com>
2024-08-23 11:36:24 +0200

    works bbut needs styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def profile
     @user = current_user
     if @user.role == "tenant"
+      @venue = Venue.new
       @my_venues = Venue.where(user_id: current_user.id)
       @my_events = Event.where(venue_id: @my_venues.ids)
     elsif @user.role == "act"
