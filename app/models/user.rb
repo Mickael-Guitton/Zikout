31c70015fa0b9e5c028e6d8aece983c379c8ea8d
Mickael Guitton <mickaelkael88@hotmail.fr>
2024-08-27 15:07:42 +0200

    change logo adn icon in navbar, search done but need to adjust

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-
-  # include PgSearch::Model
-  # multisearchable against: [:name, :city]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
